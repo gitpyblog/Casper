cc757c87475968ee07ca5837a4c32ab3948e2a0a
Dawid.S <dawid.baltica@gmail.com>
2021-10-30 11:00:09 +0200

    better Gutenberg

--- algorithms/gutenberg.py
+++ algorithms/gutenberg.py
@@ -6,15 +6,16 @@ class Gutenberg:
     @staticmethod
     def on_message(message):
         casper_id = '<@!853645195802181672>'  # id caspra
+        msg = message.content.lower()
 
-        if f'{casper_id} test' == message.content.lower():
+        if f'{casper_id} test' == msg:
             return '👻'
 
-        if f'{casper_id} message' in message.content.lower():
+        if f'{casper_id} message' in msg:
             return message
 
-        if f'{casper_id} rzuć kością' == message.content.lower():
+        if f'{casper_id} rzuć kością' == msg:
             return random.choice(range(1, 6))
 
-        if f'{casper_id} kto jest najlepszym programistą?' == message.content.lower():
+        if f'{casper_id} kto jest najlepszym programistą?' == msg:
             return 'Kacper \U0001F61B'
--- main.py
+++ main.py
@@ -28,7 +28,7 @@ async def on_ready():  # on_ready() wywoływane po połączeniu z discordem
 
 @casper.event
 async def on_message(message):  # on_message() wywoływane po nadejściu wiadomości
-    print(f'({message.channel}) {message.author}: {message.content}')  # Print wszystkich nadchodzących wiadomości
+    # print(f'({message.channel}) {message.author}: {message.content}')  # Print wszystkich nadchodzących wiadomości
 
     if message.author.bot is not True and str(message.channel) in interaction_channels:
         await message.channel.send(bot.on_message(message))
